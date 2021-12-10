a3ac2e64e44b80bc762748e9bf8c2f1c9fbafe51
Trottero <wrlxs@hotmail.com>
2021-12-10 15:25:17 +0100

    Update ANF implementation with numpy arrays

--- algorithms/basic-ANF/basicanf.py
+++ algorithms/basic-ANF/basicanf.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core.numeric import argwhere
 
 rng = np.random.default_rng()
 
@@ -15,23 +16,20 @@ class BasicANF():
         self.bitmask_length = int(np.log2(self.n) + self.r)
 
     def generate_bit_mask(self):
-        mask = []
+        mask = np.full(self.bitmask_length, False)
         for i in range(self.bitmask_length):
             # Check whether or not we assign bit to node
             if rng.random() < 0.5**(i + 1):
-                mask.append('1')
+                mask[i] = True
                 break
-            else:
-                mask.append('0')
 
-        # Append the rest of the bits
-        return "".join(mask) + "0" * (self.bitmask_length - len(mask))
+        return mask
 
     def generate_concatinated_bitmasks(self):
-        k_masks = ''
+        k_masks = np.full(self.k * self.bitmask_length, False)
         # Create k masks and concat
-        for _ in range(self.k):
-            k_masks += self.generate_bit_mask()
+        for k in range(self.k):
+            k_masks[k * self.bitmask_length: (k + 1) * self.bitmask_length] = self.generate_bit_mask()
 
         return k_masks
 
@@ -48,7 +46,7 @@ class BasicANF():
 
             # For every edge pair x y set the value of M[h][x] to the current + previous from y
             for x, y in self.edge_list:
-                M[h][x] = self.bitwise_or_for_mask(M[h][x], M[h - 1][y])
+                np.logical_or(M[h][x], M[h - 1][y], out=M[h][x])
 
             # For each node compute the approximate distance based on the k masks
             IN[h] = {node: self.approximate_distance_from_mask(M[h][node]) for node in self.nodes}
@@ -56,31 +54,21 @@ class BasicANF():
         # Return mean neighbourhood size for every distance
         return {h: np.mean(list(IN[h].values())) for h in range(1, max_distance)}, IN, M
 
-    def get_mean_least_zero_bit(self, combined_mask):
-        # Find the least significant bit that is set to 0
-        splits = self.split_by_k(combined_mask, self.bitmask_length)
-        mean = np.mean([self.get_index_of_0(split) for split in splits])
-        return mean
-
-    def get_index_of_0(self, mask):
-        if '0' in mask:
-            return mask.index('0')
-        return len(mask)
-
     def approximate_distance_from_mask(self, combined_mask):
         return 2**self.get_mean_least_zero_bit(combined_mask) / 0.77351
 
-    def split_by_k(self, seq, n):
-        '''A generator to divide a sequence into chunks of n units.'''
-        while seq:
-            yield seq[:n]
-            seq = seq[n:]
+    def get_mean_least_zero_bit(self, combined_mask):
+        # Find the least significant bit that is set to 0
 
-    def bitwise_or_for_mask(self, mask1, mask2):
-        r = ''
-        for i in range(len(mask1)):
-            if mask1[i] == "1" or mask2[i] == "1":
-                r += "1"
-            else:
-                r += "0"
-        return r
+        s = 0
+        for k in range(self.k):
+            s += self.get_index_of_0(combined_mask[k * self.bitmask_length: (k + 1) * self.bitmask_length])
+        return s / self.k
+
+    def get_index_of_0(self, mask):
+        # Find the index of the least significant bit that is set to 0
+        args = np.argwhere(mask == False)
+        if len(args) == 0:
+            return self.bitmask_length
+
+        return args[0]
