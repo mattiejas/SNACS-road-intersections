00359e396e1c1bef53ccda01cca26d28d48fb6e3
Matthias Aarnoutse <mattiejas@outlook.com>
2021-12-12 00:05:51 +0100

    Generate edgelist using main script

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-# %%
 import networkx as nx
 import json
+import sys
+import subprocess
+import os
 
-# osmium tags-filter malta-latest.osm.pbf w/highway=motorway,primary,trunk -o malta-highways.xml --overwrite
+if __name__ == '__main__':
+    country = 'netherlands'
+    if len(sys.argv) == 2:
+        country = sys.argv[1]
 
-# not working:
-# osmosis --read-xml file="data/croatia/croatia-highways.xml" --write-xml file="croatia-highways.xml" --networkx
+    dir = f'./data/{country}'
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
-# working:
-# python ../libs/OsmToRoadGraph/run.py -f croatia-highways.xml --networkx
+    if not os.path.exists(f'{dir}/{country}.osm.pbf'):
+        print(f'Downloading {country}...')
+        subprocess.run(['python3', './download.py', country])
 
-country = "croatia"
+    filter_tags_command = f"osmium tags-filter {dir}/{country}.osm.pbf w/highway=motorway,primary,trunk -o {dir}/{country}-highways.xml --overwrite"
+    subprocess.run(filter_tags_command, shell=True)
 
-file_name = f"data/{country}/{country}-highways.json"
+    create_networkx_json = f"python ./libs/OsmToRoadGraph/run.py -f {dir}/{country}-highways.xml --networkx"
+    subprocess.run(create_networkx_json, shell=True)
 
-with open(file_name) as f:
-    json_data = json.load(f)
+    file_name = f"{dir}/{country}-highways.json"
 
-G = nx.readwrite.json_graph.adjacency_graph(json_data)
-nx.write_edgelist(G, f'data/{country}/{country}-highways.csv', data=False)
-nx.info(G)
-# %%
+    with open(file_name) as f:
+        json_data = json.load(f)
+
+    G = nx.readwrite.json_graph.adjacency_graph(json_data)
+    nx.write_edgelist(G, f'{dir}/{country}-highways.csv', data=False)
+
+    create_image = f"python libs/OsmToRoadGraph/examples/pycgr-to-png/pycgr-to-png.py -f {dir}/{country}-highways.pypgr -o {dir}/{country}.png"
+    subprocess.run(create_image, shell=True)
