965fb8fe4a1e95b317319da7154c7e129febb79e
Matthias Aarnoutse <mattiejas@outlook.com>
2021-12-11 18:06:29 +0100

    measure speed

--- anf_rs/src/main.rs
+++ anf_rs/src/main.rs
@@ -1,11 +1,18 @@
 mod anf;
 
 fn main() {
+    let now = std::time::Instant::now();
+
     let edges = split_line(&std::fs::read_to_string("../edgelist.csv").unwrap());
     let mut model = anf::ANF::new(edges, 7, 128);
     let (neighbourhood_sizes, mean_sizes) = model.compute(5);
     println!("neighbourhood_sizes = {:#?}", neighbourhood_sizes);
     println!("mean = {:#?}", mean_sizes);
+
+    println!(
+        "ran for {}ms",
+        (now.elapsed().as_nanos()) as f32 / 1000000f32
+    );
 }
 
 fn split_line(line: &str) -> Vec<(usize, usize)> {
