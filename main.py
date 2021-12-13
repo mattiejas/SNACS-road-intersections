4b6c62424f5bc6b12f157a64e7728bf2058530ae
Matthias Aarnoutse <matthias@bitlag.studio>
2021-12-13 18:12:53 +0100

    Add tqdm

--- main.py
+++ main.py
@@ -6,30 +6,35 @@ import subprocess
 import os
 
 
-def ANF(country, distance=5, r=7, k=128):
+def print_if_verbose(verbose, *args):
+    if verbose:
+        print(*args)
+
+
+def ANF(country, distance=5, r=7, k=128, verbose=False):
     dir = f'./data/{country}'
     if not os.path.exists(dir):
         os.makedirs(dir)
 
     if not os.path.exists(f'{dir}/{country}.osm.pbf'):
-        print(f'Downloading {country}...')
-        subprocess.run(['python3', './download.py', country])
+        print_if_verbose(verbose, f'Downloading {country}...')
+        subprocess.run(['python3', './download.py', country], capture_output=False)
     else:
-        print(f'{country} already downloaded.')
+        print_if_verbose(verbose, f'{country} already downloaded.')
 
-    print(f'Converting {country}...')
+    print_if_verbose(verbose, f'Converting {country}...')
 
     if not os.path.exists(f'{dir}/{country}-highways.xml'):
         filter_tags_command = f"osmium tags-filter {dir}/{country}.osm.pbf w/highway=motorway,primary,trunk,motorway_link,trunk_link,primary_link -o {dir}/{country}-highways.xml --overwrite"
-        subprocess.run(filter_tags_command, shell=True)
+        subprocess.run(filter_tags_command, shell=True, capture_output=False)
     else:
-        print(f'{country}-highways.xml already exists.')
+        print_if_verbose(verbose, f'{country}-highways.xml already exists.')
 
     if not os.path.exists(f"{dir}/{country}-highways.json"):
         create_networkx_json = f"python ./libs/OsmToRoadGraph/run.py -f {dir}/{country}-highways.xml --networkx"
-        subprocess.run(create_networkx_json, shell=True)
+        subprocess.run(create_networkx_json, shell=True, capture_output=False)
     else:
-        print(f'{country}-highways.json already exists.')
+        print_if_verbose(verbose, f'{country}-highways.json already exists.')
 
     file_name = f"{dir}/{country}-highways.json"
 
@@ -40,17 +45,17 @@ def ANF(country, distance=5, r=7, k=128):
         G = nx.readwrite.json_graph.adjacency_graph(json_data)
         nx.write_edgelist(G, f'{dir}/{country}-highways.csv', data=False)
     else:
-        print(f'{country}-highways.csv already exists.')
+        print_if_verbose(verbose, f'{country}-highways.csv already exists.')
 
     if not os.path.exists(f'{dir}/{country}.png'):
         create_image = f"python libs/OsmToRoadGraph/examples/pycgr-to-png/pycgr-to-png.py -f {dir}/{country}-highways.pypgr -o {dir}/{country}.png"
-        subprocess.run(create_image, shell=True)
+        subprocess.run(create_image, shell=True, capture_output=False)
     else:
-        print(f'{country}.png already exists.')
+        print_if_verbose(verbose, f'{country}.png already exists.')
 
     # run ANF
     anf = f"./ANF {dir}/{country}-highways.csv"
-    print(f'Running ANF on {country}...')
+    print_if_verbose(verbose, f'Running ANF on {country}...')
     result = subprocess.run(anf, shell=True, stdout=subprocess.PIPE)
     return json.loads(result.stdout.decode('utf-8'))
 
@@ -73,4 +78,4 @@ if __name__ == '__main__':
     if len(sys.argv) >= 5:
         k = sys.argv[4]
 
-    ANF(country, distance, r, k)
+    ANF(country, distance, r, k, verbose=True)
